a79b7809a9d1f49905201dc727c2f45ef99b959b
Andrew Razin <razin.andrew.ru@mail.ru>
2023-04-30 20:48:36 +0300

    fixed Iterators

--- iterator/Iterator.hpp
+++ iterator/Iterator.hpp
@@ -1,18 +1,20 @@
 #include "tree.h"
 
-
 template<typename T>
 class RBTree;
 
-template <typename T>
-class RBIterator : std::iterator<std::random_access_iterator_tag, NodePtr<T>> {
-private:
+template<typename T>
+class RBIterator : std::iterator<std::random_access_iterator_tag, NodePtr<T>>
+{
+ private:
 	friend class RBTree<T>;
 	std::weak_ptr<NodePtr<T>> cur_node;
-	void prev();
-	void next();
-	void find_next_up();
-	void find_next_down();
+	const NodePtr<T> prev();
+	const NodePtr<T> next();
+	NodePtr<T> find_next_up();
+	NodePtr<T> find_next_down();
+	NodePtr<T> find_prev_up();
+	NodePtr<T> find_prev_down();
 
 	explicit RBIterator(NodePtr<T> node);
  public:
@@ -24,40 +26,39 @@ private:
 
 	T get();
 
-	const &T &operator*();
-	RBIterator& operator++();
-	RBIterator operator++(int);
+	T& operator*();
+	T* operator->();
 
-
-
+	const RBIterator operator++();
+	const RBIterator operator++(int);
 
 	// Операции, необходимые для InputIterator.
-	const &T *operator->() const;
+	const T* operator->() const;
 
 	// Операции, необходимые для BidirectionalIterator.
-	RBIterator& operator--();
-	RBIterator operator--(int);
+	const RBIterator operator--();
+	const RBIterator operator--(int);
 
-	bool operator == (const RBIterator &other);
-	bool operator != (const RBIterator &other);
+	bool operator==(const RBIterator& other);
+	bool operator!=(const RBIterator& other);
 };
 
 template<typename T>
-void RBIterator<T>::operator++()
+const RBIterator<T> RBIterator<T>::operator++()
 {
-	next();
-	return *this;
+	return next();
+
 }
 
 template<typename T>
-void RBIterator<T>::operator--()
+const RBIterator<T> RBIterator<T>::operator--()
 {
-	prev();
-	return *this;
+	return prev();
+
 }
 
 template<typename T>
-void RBIterator<T>::operator++(int)
+const RBIterator<T> RBIterator<T>::operator++(int)
 {
 	NodePtr<T> save_node_ptr = cur_node;
 	next();
@@ -65,28 +66,27 @@ void RBIterator<T>::operator++(int)
 }
 
 template<typename T>
-void RBIterator<T>::operator--(int)
+const RBIterator<T> RBIterator<T>::operator--(int)
 {
 	NodePtr<T> save_node_ptr = cur_node;
 	prev();
 	return save_node_ptr;
 }
 
-
 template<typename T>
-NodePtr RBIterator<T>::find_next_down()
+NodePtr<T> RBIterator<T>::find_next_down()
 {
-	NodePtr<T> shared_node_ptr = current_node.lock();
+	NodePtr<T> shared_node_ptr = cur_node.lock();
 	shared_node_ptr = shared_node_ptr->right;
 	while (shared_node_ptr->left != nullptr)
 		shared_node_ptr = shared_node_ptr->left;
-	return shared_node_ptr
+	return shared_node_ptr;
 }
 
 template<typename T>
-NodePtr  RBIterator<T>::find_next_up()
+NodePtr<T> RBIterator<T>::find_next_up()
 {
-	NodePtr<T> shared_node_ptr = current_node.lock();
+	NodePtr<T> shared_node_ptr = cur_node.lock();
 	NodePtr<T> parent = shared_node_ptr->parent;
 	while (parent != nullptr && shared_node_ptr == parent->right)
 	{
@@ -96,82 +96,82 @@ NodePtr  RBIterator<T>::find_next_up()
 	return parent;
 }
 
-
 template<typename T>
-void RBIterator<T>::find_prev_down()
+NodePtr<T> RBIterator<T>::find_prev_down()
 {
-	NodePtr<T> shared_node_ptr = current_node.lock();
+	NodePtr<T> shared_node_ptr = cur_node.lock();
 	shared_node_ptr = shared_node_ptr->left;
 	while (shared_node_ptr->left != nullptr)
 		shared_node_ptr = shared_node_ptr->right;
-	current_node = shared_node_ptr;
+	return shared_node_ptr;
+
 }
 
 template<typename T>
-void RBIterator<T>::find_prev_up()
+NodePtr<T> RBIterator<T>::find_prev_up()
 {
-	NodePtr<T> shared_node_ptr = current_node.lock();
+	NodePtr<T> shared_node_ptr = cur_node.lock();
 	NodePtr<T> parent = shared_node_ptr->parent;
 	while (parent != nullptr && shared_node_ptr == parent->left)
 	{
 		shared_node_ptr = parent;
 		parent = parent->parent;
 	}
-	current_node = parent;
-}
+	return parent;
 
-
-template<typename T>
-void RBIterator<T>::next()
-{
-	NodePtr<T> shared_node_ptr = current_node.lock();
-
-	//if (shared_node_ptr == nullptr)
-		//Todo::Throw out of bounds exc
-	 if (shared_node_ptr->right != nullptr)
-		find_next_down();
-	 else
-		find_next_up();
-	return *this;
 }
 
 template<typename T>
-void RBIterator<T>::prev()
+const NodePtr<T> RBIterator<T>::next()
 {
+	NodePtr<T> shared_node_ptr = cur_node.lock();
+
 	//if (shared_node_ptr == nullptr)
 	//Todo::Throw out of bounds exc
 	if (shared_node_ptr->right != nullptr)
-		find_prev_down();
+		return find_next_down();
 	else
-		find_prev_up();
+		return find_next_up();
+	//return *this;
 }
 
-
 template<typename T>
-bool RBIterator<T>::operator==(const RBIterator &other)
+const NodePtr<T> RBIterator<T>::prev()
 {
-	return this->get() == other.get()
+	//if (shared_node_ptr == nullptr)
+	//Todo::Throw out of bounds exc
+	if (cur_node->right != nullptr)
+		return find_prev_down();
+	else
+		return find_prev_up();
 }
 
 template<typename T>
-bool RBIterator<T>::operator!=(const RBIterator &other)
+bool RBIterator<T>::operator==(const RBIterator& other)
 {
-	return this->get() != other.get()
+	return this->get() == other.get();
 }
 
-
+template<typename T>
+bool RBIterator<T>::operator!=(const RBIterator& other)
+{
+	return this->get() != other.get();
+}
 
 template<typename T>
-T get()
+T RBIterator<T>::get()
 {
 	return cur_node->data;
 }
 
 template<typename T>
-const T &RBTreeIterator<T>::operator*() const
+T &RBIterator<T>::operator*()
 {
-	return this->cur_node->data;
+	return this->cur_node.lock().data;
 }
 
-
-
+template<typename T>
+T *RBIterator<T>::operator->()
+{
+	return &(this->cur_node.lock().data);
+}
